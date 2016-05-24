6707346f32d9200a4f70457ca3c3a83f853cfdb0
pabloSB27 <pnsilva@alumnos.ubiobio.cl>
2016-05-24 18:57:17 -0400

    centrado

--- Vista/formularioRegistro.php
+++ Vista/formularioRegistro.php
@@ -16,7 +16,7 @@ include('header.php');
           </div>
         </div>
           <div class="row">
-            <div class="col-sm-12">
+            <div class="col-md-12 col-md-offset-3">
               <form  method="post" action="nuevoUsuario.php" class="design-form" >
 
                 <div class="row">
